e828b12fc2e5d1d078379846dd3d3943231ecf1d
AriiiiDkCode <m.harith.hadi@gmail.com>
2025-02-18 12:44:15 +0800

    Until Chap 3

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+//Use this file to keep the fonts that will be used throughout the appliaction
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin']});
+export const lusitana = Lusitana({
+    weight: ['400','700'],
+    subsets:['latin'],
+});
